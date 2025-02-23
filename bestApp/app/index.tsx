e21a26306b19bacdfdb6ea3984732d6b8c853e6b
cfdoud <prouddoud01@gmail.com>
2025-02-23 00:32:26 -0800

    Front page tren

--- bestApp/app/index.tsx
+++ bestApp/app/index.tsx
@@ -1,16 +1,42 @@
-import { Text, View } from "react-native";
-import Tren from "../assets/images/tren.png";
+import { Text, View, Image } from "react-native";
+
+
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <img src={Tren} alt="Tren" />
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.container}>
+        <View style={styles.leftHalf}>
+          <View>          
+            <Text style={styles.text}>LEFT</Text>
+          </View>
+        </View>
+
+        <View style={styles.rightHalf}>
+        <Text style={styles.text}>Right</Text>
+        </View>
+      </View>
   );
 }
+
+const styles = { 
+  container: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  leftHalf: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#212121',
+  },
+  rightHalf: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#121212',
+  },
+};
+
