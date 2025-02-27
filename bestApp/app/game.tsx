d60a8a9fe759673c7895eb817e34712b55489792
cfdoud <prouddoud01@gmail.com>
2025-02-26 21:28:27 -0800

    game page

---
+++ bestApp/app/game.tsx
@@ -0,0 +1,13 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { JumpingTransition } from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
+
+
+export default function game() {
+    <View>
+        <Text>Game</Text>
+    </View>
+}
+//                 fontSize: 18.26,
+//                 color: "#FFFFFF",
